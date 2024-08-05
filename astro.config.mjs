72d24a0aea1991f2d2995acc8240e9c2998cac82
Anthony Baker <bakera81@icloud.com>
2024-08-05 15:43:04 -0400

    Install react to use react components, use the raw embed instead for Fillout

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,13 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import netlify from "@astrojs/netlify";
+// import react from '@astrojs/react'; // For using Fillout form components
+
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://lexingtonthemes.com/",
+  // integrations: [tailwind(), sitemap(), react()],
   integrations: [tailwind(), sitemap()],
   output: "hybrid",
   adapter: netlify()
