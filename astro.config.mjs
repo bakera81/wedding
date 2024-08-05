7f042935882be1a639d88dae5a2cf744ce9c75aa
Anthony Baker <bakera81@icloud.com>
2024-08-05 13:21:57 -0400

    use hybrid SSR w/ Netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
+import netlify from "@astrojs/netlify";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://lexingtonthemes.com/",
-  integrations: [tailwind(), sitemap()]
+  integrations: [tailwind(), sitemap()],
+  output: "hybrid",
+  adapter: netlify()
 });
